738b060185ee55ba792ea54d2967001b04c6b5b1
Mateusz Moskwa <mateuszmos18@gmail.com>
2015-05-17 22:40:00 +0200

    Adds some institiutes controller actions

--- app/controllers/institutes_controller.rb
+++ app/controllers/institutes_controller.rb
@@ -4,21 +4,42 @@ class InstitutesController < ApplicationController
 	# before_filter :authenticate
 
 	def index
-
+		@institutes = Institute.all.order(:name)
 	end
 
 	def show
 	end
 
+	def edit
+		@institute = Institute.find(params[:id])
+		@lecturers = @institute.lecturers
+		@employments = @institute.employments
+	end
+
+	def new
+	end
+
+	def create
+		institute = Institute.new(institute_params)
+		if institute.valid?
+			institute.save
+			redirect_to institutes_path
+		else
+			# TODO
+		end
+
+	end
+
 	private
+
 		def authenticate
 			authenticate_or_request_with_http_digest do |username|
 				USERS[username]
 			end
 		end
 
-		# def person_params
-		# 	params.require(:person)
-		# end
+		def institute_params
+			params.require(:institute).permit(:name)
+		end
 
 end
