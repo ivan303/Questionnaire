85fa0b51c0ecfb6bcbe5e928750161164f160d8c
Mateusz Moskwa <mateuszmos18@gmail.com>
2015-05-20 23:57:07 +0200

    Adds form views (index, show); cascade deleting votes

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -3,6 +3,17 @@ class FormsController < ApplicationController
   layout "admin"
   before_filter :authenticate, except: :create
 
+  def index
+    @forms = Form.institute_search(params[:institute_search])
+                 .ip_search(params[:ip_search])
+                 .custom_sort(params[:sort])
+                 .paginate(:per_page => 30, :page => params[:page])
+  end
+
+  def show
+    @form = Form.find(params[:id])
+  end
+
   def create
     questions = Question.all.as_json
     institute = Institute.find(params[:institute_id])
@@ -22,6 +33,12 @@ class FormsController < ApplicationController
       # TODO
     end
     redirect_to polls_path
+  end
+
+  def destroy
+    form = Form.find(params[:id])
+    form.destroy
+    redirect_to forms_path
   end
 
   private
--- app/models/form.rb
+++ app/models/form.rb
@@ -1,4 +1,48 @@
 class Form < ActiveRecord::Base
-  has_many :votes
+  has_many :votes, :dependent => :destroy
   accepts_nested_attributes_for :votes
+
+  def self.institute_search (search)
+    unless search.blank?
+      search.downcase!
+      Form.joins('JOIN votes ON forms.id = votes.form_id JOIN institutes ON institutes.id = votes.institute_id ')
+          .where('LOWER(institutes.name) LIKE ?', "%#{search}%")
+          .group('forms.id')
+    else
+      Form.all
+    end
+  end
+
+  def self.ip_search (search)
+    unless search.blank?
+      Form.joins('JOIN votes ON forms.id = votes.form_id')
+          .where('ip LIKE ?', "%#{search}%")
+          .group('forms.id')
+    else
+      Form.all
+    end
+  end
+
+  def self.custom_sort (sort_by)
+    unless sort_by.blank?
+      # byebug
+      # Vote.all
+      case sort_by
+        when "institute_id" then Form.joins('JOIN votes ON forms.id = votes.form_id JOIN institutes ON institutes.id = votes.institute_id ')
+                                     .group('forms.id, institutes.name')
+                                     .order('institutes.name')
+                                     # .group('forms.id')
+
+        when "ip" then Form.joins('JOIN votes ON forms.id = votes.form_id')
+                           .group('forms.id, votes.ip')
+                           .order('votes.ip')
+
+
+      end
+    else
+      # byebug
+      Form.all
+    end
+  end
+
 end
