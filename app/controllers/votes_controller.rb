1508cd711b24ddc82dadf84e8ab6c058e2542110
Mateusz Moskwa <mateuszmos18@gmail.com>
2015-05-20 15:08:45 +0200

    Adds pagination to votes and searching mechanism

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,12 @@
 class VotesController < ApplicationController
 
   def index
-    @votes = Vote.all
+    @votes = Vote.lastname_search(params[:lastname_search])
+                 .firstname_search(params[:firstname_search])
+                 .institute_search(params[:institute_search])
+                 .ip_search(params[:ip_search])
+                 .paginate(:per_page => 20, :page => params[:page])
   end
+
+
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,41 @@ class Vote < ActiveRecord::Base
   belongs_to :institute
   belongs_to :question
   belongs_to :form
+
+  def self.lastname_search (search)
+    unless search.empty?
+      search.downcase!
+      Vote.joins('JOIN lecturers ON lecturers.id = votes.lecturer_id').where('LOWER(lecturers.lastname) LIKE ?', "%#{search}%")
+    else
+      Vote.all
+    end
+  end
+
+  def self.firstname_search (search)
+    unless search.empty?
+      search.downcase!
+      Vote.joins('JOIN lecturers ON lecturers.id = votes.lecturer_id').where('LOWER(lecturers.firstname) LIKE ?', "%#{search}%")
+    else
+      Vote.all
+    end
+  end
+
+  def self.institute_search (search)
+    unless search.empty?
+      search.downcase!
+      Vote.joins('JOIN institutes ON institutes.id = votes.institute_id').where('LOWER(institutes.name) LIKE ?', "%#{search}%")
+    else
+      Vote.all
+    end
+  end
+
+  def self.ip_search (search)
+    unless search.empty?
+      Vote.where('ip LIKE ?', "%#{search}%")
+    else
+      Vote.all
+    end
+  end
+
+
 end
